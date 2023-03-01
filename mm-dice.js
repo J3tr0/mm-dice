5180f1c8a7c8c40990722e4631cb7c00eeeef4e2
Fabio Giannace <fabio.giannace@gmail.com>
2023-03-01 17:22:18 +0100

    add chatmessage for results

--- mm-dice.js
+++ mm-dice.js
@@ -5,6 +5,48 @@ Hooks.once('init', async function () {
 	CONFIG.Dice.terms['b'] = DieMMBlack;
 });
 
+Hooks.on('diceSoNiceRollComplete', (chatMessageID) => {
+	let message = game.messages.get(chatMessageID);
+	if (message.isAuthor) {
+		let success = 0;
+		let critical = 0;
+		let failure = 0;
+		let mmRoll = false;
+		message.roll.dice.forEach((dice) => {
+			if (dice instanceof DieMMWhite || dice instanceof DieMMBlack) {
+				mmRoll = true;
+				dice.results.forEach((res) => {
+					switch (res.result) {
+						case 1:
+							if (dice instanceof DieMMBlack) failure++;
+							break;
+						case 5:
+							success++;
+							break;
+						case 6:
+							if (dice instanceof DieMMBlack) critical++;
+							else success++;
+							break;
+					}
+				});
+			}
+		});
+
+		if (mmRoll) {
+			let total = critical * 3 + success - failure;
+			let content =
+				critical > 0
+					? `Critical: <b>${total}</b>`
+					: `Success: <b>${total}</b>`;
+			ChatMessage.create({
+				content: content,
+				whisper: message.data.whisper,
+				blind: message.data.blind,
+			});
+		}
+	}
+});
+
 Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addSystem({ id: 'memento-mori', name: 'Memento Mori' }, false);
 	dice3d.addDicePreset(
