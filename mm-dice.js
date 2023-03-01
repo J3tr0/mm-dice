bb3133e844536b2b094f4a71e3c58b01af54aee0
Fabio Giannace <fabio.giannace@gmail.com>
2023-03-01 08:34:10 +0100

    first release

--- mm-dice.js
+++ mm-dice.js
@@ -2,23 +2,51 @@ import { DieMMBlack, DieMMWhite } from './die.js';
 
 Hooks.once('init', async function () {
 	CONFIG.Dice.terms['w'] = DieMMWhite;
-});
-
-Hooks.once('init', async function () {
 	CONFIG.Dice.terms['b'] = DieMMBlack;
 });
 
 Hooks.once('diceSoNiceReady', (dice3d) => {
-	dice3d.addSystem({ id: 'memento-white', name: 'Memento Mori White' }, true);
-	dice3d.addDicePreset({
-		type: 'mmw',
-		system: 'memento-white',
-	});
-});
-Hooks.once('diceSoNiceReady', (dice3d) => {
-	dice3d.addSystem({ id: 'memento-black', name: 'Memento Mori Black' }, true);
-	dice3d.addDicePreset({
-		type: 'mmb',
-		system: 'memento-black',
-	});
+	dice3d.addSystem({ id: 'memento-mori', name: 'Memento Mori' }, false);
+	dice3d.addDicePreset(
+		{
+			type: 'dw',
+			labels: ['1', '2', '3', '4', '5', '6'],
+			colorset: 'mm-white',
+			system: 'memento-mori',
+		},
+		'd6'
+	);
+	dice3d.addDicePreset(
+		{
+			type: 'db',
+			labels: ['1', '2', '3', '4', '5', '6'],
+			colorset: 'mm-black',
+			system: 'memento-mori',
+		},
+		'd6'
+	);
+	dice3d.addColorset(
+		{
+			name: 'mm-white',
+			description: 'MM White',
+			category: 'Colors',
+			foreground: '#000000',
+			background: '#FFFFFF',
+			texture: 'none',
+			edge: '#FFFFFF',
+		},
+		'no'
+	);
+	dice3d.addColorset(
+		{
+			name: 'mm-black',
+			description: 'MM Black',
+			category: 'Colors',
+			foreground: '#FFFFFF',
+			background: '#000000',
+			texture: 'none',
+			edge: '#000000',
+		},
+		'no'
+	);
 });
